918306e91690716886a57a0780eab3f0702af7c0
James Dabbs <jdabbs@emcien.com>
2013-01-11 09:04:35 -0500

    Initial commit. Empty project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lagrange::Application.config.secret_token = '89ef9dcf7e3124c6e22b6373da4bfdfc56d68866c7ebb9609c58650a8dbe3e6aeb1f33c43a9b715c9d0ea7567e44ffda3440e5deffe7c559e12768bcee9fa6ae'
