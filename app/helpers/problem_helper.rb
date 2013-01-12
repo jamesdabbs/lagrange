af763f639219efd0f2455cc38edba49ff3036fb3
James Dabbs <jdabbs@emcien.com>
2013-01-12 16:37:40 -0500

    Bootstrap styles.

--- app/helpers/problem_helper.rb
+++ app/helpers/problem_helper.rb
@@ -3,4 +3,14 @@ module ProblemHelper
     solution = problem.solution(language)
     render partial: "solution_summary", locals: { solution: solution }
   end
+
+  def speed_info(solution)
+    rt = solution.runtime
+    return unless rt
+    if rt.to_f > 60
+      "<span class='badge badge-warning'>#{rt}</span>".html_safe
+    else
+      rt
+    end
+  end
 end
